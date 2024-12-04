19da8f9cfe038b259436dfe790929102ff15ff77
Alemoretti <alemoretti10@gmail.com>
2024-12-04 14:27:16 -0300

    Create updateProfile endpoint

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -66,5 +66,33 @@ export const login = async (req, res) => {
 }
 
 export const logout = (req, res) => {
-  res.send("Logout route")
+  try {
+    res.cookie("jwt", "", {
+      masAge: 0
+    })
+    res.status(200).json({ message: "Logged out succesfully" })
+  } catch (error) {
+    console.log("Error in logout controller", error.message)
+    res.status(500).json({ message: "Internal server error" })
+    
+  }
+}
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body
+    const userId = req.user._id
+
+    if (!profilePic) return res.status(400).json({ message: "Profile picture is required" })
+
+    const uploadResponse = cloudinary.uploader.upload(profilePic)
+    const updatedUser = await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true })
+
+    res.status(200).json({ message: updatedUser})
+
+  } catch (error) {
+    console.log("Error in updateProfile controller", error.message)
+    res.status(500).json({ message: "Internal server error" })
+    
+  }
 }
