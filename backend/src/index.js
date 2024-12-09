178627d39f7d94b9b9d66174b1e299157594fdf3
Alemoretti <alemoretti10@gmail.com>
2024-12-09 06:23:06 -0300

    Create message model and controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
 import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser"
 
@@ -11,7 +12,9 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT)
